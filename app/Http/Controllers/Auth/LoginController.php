a515a058cbf0d17a224742a61e76efa9d0a88daf
emman <torrestorresemmanuel@gmail.com>
2021-02-09 15:32:10 -0600

    Normal

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -9,7 +9,6 @@ use Illuminate\Http\Response;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Client;
 use Auth;
-use App\User;
 
 class LoginController extends Controller
 {
@@ -64,19 +63,14 @@ class LoginController extends Controller
             'password'           => 'required',
             'g-recaptcha-response' => 'required|captcha'
         ]);
-        $usuario = User::where("email",$request->email)->first();
-        echo json_encode($usuario);
-        /*
         if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
             // Success
             return redirect()->intended('/');
         } else {
           return Auth::attempt(['email' => $request->email, 'password' => $request->password]);
             // Go back on error (or do what you want)
-
             return redirect()->back();
         }
-        */
 
     }
 
