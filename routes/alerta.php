fd195fa752da53c50c565fb50271f2ca1a59a48d
Obed Martinez <martinezobednoe@gmail.com>
2021-02-09 18:30:31 -0600

    Router Alertas

--- routes/alerta.php
+++ routes/alerta.php
@@ -2,3 +2,6 @@
 
 Route::get('alertas', 'Alerta@todos');
 Route::post('nuevaAlerta', 'Alerta@nueva');
+Route::get('encontrar/{id}', 'Alerta@encontrar');
+Route::put('editar/{id}', 'Alerta@editar');
+Route::delete('eliminar/{id}', 'Alerta@eliminar');
