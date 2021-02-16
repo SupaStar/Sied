ea0b2d5427d29f9bfd89b15f2cbd09d66c511268
Obed Martinez <martinezobednoe@gmail.com>
2021-02-15 22:02:48 -0600

    Consumo de api de banxico

--- app/ConfigAlertas.php
+++ app/ConfigAlertas.php
@@ -2,10 +2,31 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class ConfigAlertas extends Model
 {
-  protected $table = "alertas_pld";
+  protected $table = "configuracionalerta";
 
+  public function valorUID()
+  {
+    $db = ConfigAlertas::find(1);
+    $fechaBd=Carbon::make($db->actualizacionUid)->format("Y-m-d");
+    if($fechaBd<Carbon::now()){
+      $hoy = Carbon::now()->format("Y-m-d");
+      $endpoint = "https://www.banxico.org.mx/SieAPIRest/service/v1/series/SP68257/datos/" . $hoy . "/" . $hoy;
+      $client = curl_init();
+      curl_setopt($client, CURLOPT_URL, $endpoint);
+      curl_setopt($client, CURLOPT_HTTPHEADER, array(
+        'Bmx-Token:ba350db5f68fde993e7979d888bef54441414955063870c0fc04681ec87a86bd'
+      ));
+      curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+      $data = json_decode(curl_exec($client));
+      curl_close($client);
+      $db->valor = $data->bmx->series[0]->datos[0]->dato;
+      $db->actualizacionUid = Carbon::now();
+      $db->save();
+    }
+  }
 }
---
+++ database/migrations/2021_02_15_204837_alter_configuracionuid_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterConfiguracionuidTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::table('configuracionalerta', function (Blueprint $table) {
+        $table->decimal("valor");
+        $table->dateTime('actualizacionUid')->nullable();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
