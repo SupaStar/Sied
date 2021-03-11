24ce00a2b1e4746897d5c1e8eaec23411e83e48b
ObedNoe22 <obednoe22@gmail.com>
2021-03-10 21:35:09 -0600

    Update 2021_03_10_164309_create_perfil_transacional_moral_table.php

--- database/migrations/2021_03_10_164309_create_perfil_transacional_moral_table.php
+++ database/migrations/2021_03_10_164309_create_perfil_transacional_moral_table.php
@@ -40,14 +40,14 @@ class CreatePerfilTransacionalMoralTable extends Migration
             $table->unsignedBigInteger('efr')->default(2);
             $table->float('ingreso',10,2);
             $table->integer('nPagoMes')->default(1);
-            $table->foreign('id_moral')->references('id')->on('morales');
-            $table->foreign('origen_recursos')->references('id')->on('origen_recursos');
-            $table->foreign('destino_recursos')->references('id')->on('destino_recursos');
-            $table->foreign('instrumento_monetario')->references('id')->on('instrumento_monetario');
-            $table->foreign('divisas')->references('id')->on('divisa');
-            $table->foreign('profesion')->references('id')->on('profesion');
-            $table->foreign('actividad_giro')->references('id')->on('actividad_giro');
-            $table->foreign('efr')->references('id')->on('entidad_federativa_residencia');
+//            $table->foreign('id_moral')->references('id')->on('morales');
+//            $table->foreign('origen_recursos')->references('id')->on('origen_recursos');
+//            $table->foreign('destino_recursos')->references('id')->on('destino_recursos');
+//            $table->foreign('instrumento_monetario')->references('id')->on('instrumento_monetario');
+//            $table->foreign('divisas')->references('id')->on('divisa');
+//            $table->foreign('profesion')->references('id')->on('profesion');
+//            $table->foreign('actividad_giro')->references('id')->on('actividad_giro');
+//            $table->foreign('efr')->references('id')->on('entidad_federativa_residencia');
             $table->timestamps();
         });
     }
