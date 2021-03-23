a26e7b46adb1b0cef5b518f86cf47f9f8b59e57c
Lokillo1011 <49875696+Lokillo1011@users.noreply.github.com>
2021-03-22 19:27:36 -0600

    cambios cliente

--- routes/api.php
+++ routes/api.php
@@ -1,63 +1,65 @@
 <?php
+
 Route::group([
-  'prefix' => 'auth'
-], function () {
-  Route::post('login', 'AuthController@login');
-  Route::post('register', 'AuthController@register');
-
-  Route::group([
-    'middleware' => 'auth:api'
+    'prefix' => 'auth'
   ], function () {
-    Route::get('logout', 'AuthController@logout');
-    Route::get('user', 'AuthController@user');
-    Route::get('checksession', 'AuthController@checksession');
-    Route::group([
-      'prefix' => 'create'
-    ], function () {
-      Route::post('dir', 'CreateController@dir');
-      Route::post('eventos', 'CreateController@eventos');
-      Route::post('productos', 'CreateController@productos');
-      Route::post('anuncios', 'CreateController@anuncios');
-      Route::post('uploadImages', 'CreateController@uploadImages');
-    });
+    Route::post('login', 'AuthController@login');
+    Route::post('register', 'AuthController@register');
 
     Route::group([
-      'prefix' => 'tags'
-    ], function () {
-      Route::post('addfav/{type}/{id}', 'CreateController@addfav');
-      Route::post('addview/{type}/{id}', 'CreateController@addview');
-      Route::post('addshare/{type}/{id}', 'CreateController@addshare');
-      Route::post('addcomments/{type}/{id}', 'CreateController@addcomments');
+      'middleware' => 'auth:api'
+    ], function() {
+        Route::get('logout', 'AuthController@logout');
+        Route::get('user', 'AuthController@user');
+        Route::get('checksession', 'AuthController@checksession');
+
+        Route::group([
+          'prefix' => 'create'
+        ], function () {
+          Route::post('dir', 'CreateController@dir');
+          Route::post('eventos', 'CreateController@eventos');
+          Route::post('productos', 'CreateController@productos');
+          Route::post('anuncios', 'CreateController@anuncios');
+          Route::post('uploadImages', 'CreateController@uploadImages');
+        });
+
+        Route::group([
+          'prefix' => 'tags'
+        ], function () {
+          Route::post('addfav/{type}/{id}', 'CreateController@addfav');
+          Route::post('addview/{type}/{id}', 'CreateController@addview');
+          Route::post('addshare/{type}/{id}', 'CreateController@addshare');
+          Route::post('addcomments/{type}/{id}', 'CreateController@addcomments');
+        });
+
+        Route::group([
+          'prefix' => 'mydata'
+        ], function () {
+          Route::post('delete/{type}/{id}', 'DataController@delete');
+          Route::get('favs', 'DataController@favs');
+          Route::get('prombus/{id}', 'DataController@prombus');
+          Route::get('mybusiness', 'DataController@mybusiness');
+          Route::get('business', 'DataController@business');
+          Route::get('products', 'DataController@products');
+          Route::get('events', 'DataController@events');
+          Route::get('ads', 'DataController@ads');
+          Route::get('seebusiness/{id}', 'DataController@seebusiness');
+          Route::get('seeevents/{id}', 'DataController@seeevents');
+          Route::get('seeproducts/{id}', 'DataController@seeproducts');
+          Route::get('seeads/{id}', 'DataController@seeads');
+          Route::get('lastedbusiness', 'DataController@lastedbusiness');
+          Route::get('lastedbillboard', 'DataController@lastedbillboard');
+          Route::get('lastedproduct', 'DataController@lastedproduct');
+          Route::get('lastedadd', 'DataController@lastedadd');
+          Route::get('statics/{type}/{id}', 'DataController@mystatics');
+          Route::get('listbusiness/{type}/{category}/{subcategory}', 'DataController@listbusiness');
+          Route::get('listads/{type}/{category}/{subcategory}', 'DataController@listbusiness');
+          Route::get('get/{type}/{id}', 'DataController@getdata');
+
+        });
+
     });
-
-    Route::group([
-      'prefix' => 'mydata'
-    ], function () {
-      Route::post('delete/{type}/{id}', 'DataController@delete');
-      Route::get('favs', 'DataController@favs');
-      Route::get('prombus/{id}', 'DataController@prombus');
-      Route::get('mybusiness', 'DataController@mybusiness');
-      Route::get('business', 'DataController@business');
-      Route::get('products', 'DataController@products');
-      Route::get('events', 'DataController@events');
-      Route::get('ads', 'DataController@ads');
-      Route::get('seebusiness/{id}', 'DataController@seebusiness');
-      Route::get('seeevents/{id}', 'DataController@seeevents');
-      Route::get('seeproducts/{id}', 'DataController@seeproducts');
-      Route::get('seeads/{id}', 'DataController@seeads');
-      Route::get('lastedbusiness', 'DataController@lastedbusiness');
-      Route::get('lastedbillboard', 'DataController@lastedbillboard');
-      Route::get('lastedproduct', 'DataController@lastedproduct');
-      Route::get('lastedadd', 'DataController@lastedadd');
-      Route::get('statics/{type}/{id}', 'DataController@mystatics');
-      Route::get('listbusiness/{type}/{category}/{subcategory}', 'DataController@listbusiness');
-      Route::get('listads/{type}/{category}/{subcategory}', 'DataController@listbusiness');
-      Route::get('get/{type}/{id}', 'DataController@getdata');
-
-    });
-
   });
-});
 
 
 Route::group([
