803aa8f8740430db24f4f30298e5ffd257f1dd59
Obed Martinez <martinezobednoe@gmail.com>
2021-03-25 12:54:59 -0600

    Solucion error pagos

--- app/Http/Controllers/Morales.php
+++ app/Http/Controllers/Morales.php
@@ -2526,7 +2526,7 @@ class Morales extends Controller
 
     $result = DB::SELECT("SELECT periodo,fpago,0 as mora,0 as imora,0 as condonacion, 0 as iva,pago, `full`
                           FROM `pago_morales`
-                          LEFT JOIN comprobante_pago on pago_moral_id=pagos.id
+                          LEFT JOIN comprobante_pagos_morales on pago_moral_id=pago_morales.id
                           WHERE credito_id=$creditoid");
 
     return datatables()->of($result)
