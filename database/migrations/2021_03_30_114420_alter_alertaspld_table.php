20da1443c846b046f768015e89a2f55f90a4a6bd
Obed Martinez <martinezobednoe@gmail.com>
2021-03-30 11:47:28 -0600

    Campos alertas

---
+++ database/migrations/2021_03_30_114420_alter_alertaspld_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterAlertaspldTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::table('alertas_pld', function (Blueprint $table) {
+        $table->dropColumn('titulo');
+        $table->dropColumn('descripcion');
+        $table->dropColumn('estatus');
+        $table->dropColumn('observacion');
+        $table->dropColumn('prioridad');
+      });
+      Schema::table('alertas_pld', function (Blueprint $table) {
+        $table->text('titulo');
+        $table->text('descripcion');
+        $table->integer('estatus');
+        $table->text('observacion')->nullable();
+        $table->text('prioridad');
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
